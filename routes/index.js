ec9ab7e53905e2a40f152566101d872852727dad
Kinsey Ashby <kinseyzaire@gmail.com>
2016-01-11 21:35:45 -0700

    added CSS and HTML

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// var knex = require('knex')({
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL || 'postgres://localhost/library'
+//   });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'gRestaurants' });
 });
 
 module.exports = router;
