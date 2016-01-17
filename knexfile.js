e493e26afb27d7557e32d701742f60ebbb28eb99
Kinsey Ashby <kinseyzaire@gmail.com>
2016-01-17 08:59:39 -0700

    starting in on adding new requirements

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/gtablesss'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,79 @@
 var express = require('express');
+var knex = require('../knex.js');
 var router = express.Router();
 
-// var knex = require('knex')({
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL || 'postgres://localhost/library'
-//   });
+function Restaurants() {
+   return knex('restaurants');
+}
+
+function Employees() {
+   return knex('employees');
+}
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gRestaurants' });
+  Restaurants().select().then(function(results){
+    res.render('restaurants/index', {restaurants: results});
+  });
+});
+
+router.get('/restaurants', function(req, res, next) {
+  Restaurants().select().then(function(results){
+    res.render('restaurants/index', {restaurants: results});
+  });
+});
+
+router.get('/restaurants/new', function(req, res, next) {
+  res.render('restaurants/new');
+});
+
+router.post('/restaurants', function(req, res, next) {
+  Restaurants().insert(req.body).then(function(result){
+    res.redirect('/restaurants');
+  });
+});
+
+router.get('/restaurants/:id', function (req, res, next) {
+  Restaurants().where('id', req.params.id).first().then(function(result){
+    res.render('restaurants/show', { restaurant: result });
+  });
+})
+
+router.get('/restaurants/:id/edit', function (req, res) {
+  Restaurants().where('id', req.params.id).first().then(function(result){
+    res.render('restaurants/edit', { restaurant: result });
+  });
+})
+
+router.post('/restaurants/:id', function (req, res) {
+  Restaurants().where('id', req.params.id).update(req.body)
+  .then(function(result){
+    res.redirect('/restaurants');
+  });
+});
+
+router.post('/restaurants/:id/delete', function (req, res) {
+  Restaurants().where('id', req.params.id).del()
+  .then(function (result) {
+    res.redirect('/restaurants');
+  })
+})
+
+// admin pages
+
+router.get('/admin', function(req, res, next) {
+   Restaurants().select().then(function(results){
+      res.render('admin/index', {restaurants: results, employees: results});
+   });
+});
+
+router.get('/admin/new', function(req, res, next) {
+  res.render('admin/new-employee');
+});
+
+router.post('/admin', function(req, res, next) {
+  Employees().insert(req.body).then(function(result){
+    res.redirect('/admin/index');
+  });
 });
 
 module.exports = router;
