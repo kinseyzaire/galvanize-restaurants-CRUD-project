8cceb3e9de367fe2c8adb35a706e50ba60869ed0
Kinsey Ashby <kinseyzaire@gmail.com>
2016-01-21 20:58:52 -0700

    added neighborhoods routes

--- routes/neighborhoods.js
+++ routes/neighborhoods.js
@@ -8,23 +8,54 @@ function Neighborhoods() {
 }
 
 // list all neighborhoods
-
+// var epicenters = [];
+//
 // router.get('/neighborhoods', function(req, res, next) {
-//   Neighborhoods().select().then(function(result){
-//         // console.log(jase.results[0].geometry.location);
-//         res.render('neighborhoods/index', {neighborhood: result, location: epicenter});
-//       }
-//     })
+//   Neighborhoods().select().then(function(results){
+//     for (var i = 0; i < results.length; i++) {
+//       var google_api = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+//       var address = (results[i].center).replace(/ /g, "+");
+//       var my_key = "&key=AIzaSyBWq3Gz3IlIWdXeKYBlNubGRBWd-ENdIno";
+//       request(google_api+address+my_key, function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//           var jase = JSON.parse(body);
+//           epicenters.push(jase.results[0].geometry.location);
+//         }
+//       })
+//     }).then(function(results){
+//   res.render('neighborhoods/index', {neighborhoods: results, locations: epicenters});
 //   });
 // });
 
-// list a single neighborhood
+
+router.get('/neighborhoods', function(req, res, next) {
+  Neighborhoods().select().then(function(results){
+    res.render('neighborhoods/index', {neighborhoods: results});
+  });
+});
+
+// new neighborhood page
+
+router.get('/neighborhoods/new', function(req, res, next) {
+  res.render('neighborhoods/new');
+});
+
+// add new neighborhood
+
+router.post('/neighborhoods', function(req, res, next) {
+  Neighborhoods().insert(req.body).then(function(result){
+    res.redirect('/neighborhoods/');
+  });
+});
+
+// show neighborhood
 
 router.get('/neighborhoods/:id', function(req, res, next) {
   Neighborhoods().where('id', req.params.id).then(function(result){
-    console.log(result[0]);
     var google_api = "https://maps.googleapis.com/maps/api/geocode/json?address=";
     var address = (result[0].center).replace(/ /g, "+");
+    console.log(req.params.id);
+    console.log(address);
     var my_key = "&key=AIzaSyBWq3Gz3IlIWdXeKYBlNubGRBWd-ENdIno";
     var nresult = result[0];
     request(google_api+address+my_key, function (error, response, body) {
@@ -39,57 +70,30 @@ router.get('/neighborhoods/:id', function(req, res, next) {
   });
 });
 
-// new neighborhood page
-
-router.get('/restaurants/new', function(req, res, next) {
-  res.render('restaurants/new');
-});
-
-// add new neighborhood
-
-router.post('/restaurants', function(req, res, next) {
-  Neighborhoods().insert(req.body).then(function(result){
-    res.redirect('/restaurants');
-  });
-});
-
-// show neighborhood
-
-router.get('/restaurants/:id', function (req, res, next) {
-  Restaurants().where('id', req.params.id).first().then(function(rresult){
-    Employees().where('restaurant_id', req.params.id).then(function(eresults){
-      Reviews().where('restaurant_id', req.params.id).then(function(cresults){
-        res.render('restaurants/show', { restaurant: rresult, employees: eresults, reviews: cresults });
-      });
-    });
-  });
-});
-
 // edit neighborhood
 
-router.get('/restaurants/:id/edit', function (req, res) {
-  Restaurants().where('id', req.params.id).first().then(function(result){
-    res.render('restaurants/edit', { restaurant: result });
+router.get('/neighborhoods/:id/edit', function (req, res) {
+  Neighborhoods().where('id', req.params.id).first().then(function(result){
+    res.render('neighborhoods/edit', { neighborhood: result });
   });
 });
 
 // update - send edit form
 
-router.post('/restaurants/:id', function (req, res) {
-  Restaurants().where('id', req.params.id).update(req.body)
+router.post('/neighborhoods/:id', function (req, res) {
+  Neighborhoods().where('id', req.params.id).update(req.body)
   .then(function(result){
-    res.redirect('/restaurants');
+    res.redirect('/neighborhoods');
   });
 });
 
 // delete a neighborhood
 
-router.post('/restaurants/:id/delete', function (req, res) {
-  Restaurants().where('id', req.params.id).del()
+router.get('/neighborhoods/:id/delete', function (req, res) {
+  Neighborhoods().where('id', req.params.id).del()
   .then(function (result) {
-    res.redirect('/restaurants');
+    res.redirect('/neighborhoods');
   });
 });
-
 
 module.exports = router;
